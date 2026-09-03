3eb50ebb86d1fb19087f529ecadeb54a889ad3e0
Romans.Silins <mrsnake934@gmail.com>
2026-09-03 14:34:57 +0300

    smert

---
+++ app/src/main/java/com/example/receptes_plepis_silins/ReciepeBook.java
@@ -0,0 +1,41 @@
+package com.example.receptes_plepis_silins;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.receptes_plepis_silins.databinding.ReciepeBookBinding;
+
+public class ReciepeBook extends Activity {
+    private ReciepeBookBinding binding;
+
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        binding = ReciepeBookBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.loginButton2.setOnClickListener(v ->
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment)
+        );
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
+}
